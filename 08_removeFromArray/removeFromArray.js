3ef42922902686962deeef82a5d2087b092b616f
TheRookiexe <mishrxa36@gmail.com>
2025-09-03 01:48:40 +0530

    Remove From Array Exercise Completed and all tests passed!

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valsToRemove) {
+    // index = arr.findIndex(num=> num===n);
+    // arr.splice(index,1);
+    // return arr;
+    return arr.filter(ele => !valsToRemove.includes(ele));
+
 };
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
