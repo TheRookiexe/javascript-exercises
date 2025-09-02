3911fd56a659bc7286ea2f137136607bbe0c5fd0
TheRookiexe <mishrxa36@gmail.com>
2025-09-03 01:30:02 +0530

    All tests passed and exercise done.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    return s.split("").reverse().join("");
 };
+
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc! Hello, Odinite.');
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
