2c11079036e3543febdf9d83b623009cf8a9f730
TheRookiexe <mishrxa36@gmail.com>
2025-09-03 01:18:18 +0530

    All tests passed and Exercise Completed.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(s,n) {
+    let res="";
+    if (n<0){
+        return "ERROR";
+    }
+    for(let i=0; i<n; i++){
+        res+=s+"";
+    }
+    return res;
 };
+
+repeatString("Hey", 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('', 10)
 
 // Do not edit below this line
 module.exports = repeatString;
