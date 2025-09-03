ae7d5793f54a444677f4e7a87371ff6dfebba080
TheRookiexe <mishrxa36@gmail.com>
2025-09-03 14:55:56 +0530

    tempConversion Exercise Completed.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftemp) {
+  ctmp=(ftemp-32)*(5/9);
+  return Math.round(ctmp*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctemp) {
+  ftmp=(ctemp*(9/5)+32);
+  return Math.round(ftmp*10)/10;
 };
 
 // Do not edit below this line
