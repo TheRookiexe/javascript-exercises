05aad641c394b2cc972d19be4ad5325a5887dcec
TheRookiexe <mishrxa36@gmail.com>
2025-09-03 13:10:44 +0530

    sumAll Exercise Completed.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a,n) {
+    let sum=0;
+    if (Number.isInteger(a) && Number.isInteger(n) && a>=1 && n>=1){
+        if (a>n){
+            let temp=a;
+            a = n;
+            n =  temp;
+        }
+        for(let i = a; i<=n; i++){
+            sum+=i;
+        }   
+        return sum;
+        
+    }else{
+        return "ERROR";
+    }
 };
+
+sumAll(2, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
