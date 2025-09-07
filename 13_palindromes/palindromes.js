bebf86fd97334267a7df355595ad67d8dac39989
TheRookiexe <mishrxa36@gmail.com>
2025-09-07 20:38:23 +0530

    Palindromes exercise Completed.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (Str, i=0) {
+    const cleaned = Str.toLowerCase().replace(/[^a-z0-9]/g, '')
+    let len = cleaned.length;
+    
+    if (i>=len/2){
+        return true;
+    }
+    if (cleaned[i]!==cleaned[len-1-i]){
+        return false;
+    }
+    return palindromes(cleaned, i+1);
 };
 
 // Do not edit below this line
