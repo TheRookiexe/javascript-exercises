04bf916898e9d60e4f1bdd30e658290e1284ab26
TheRookiexe <mishrxa36@gmail.com>
2025-09-07 21:28:53 +0530

    16th Exercise Completed

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function getAge(death, birth){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death-birth
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current)=>{
+        let oldestAge= getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        let currentAge= getAge(current.yearOfDeath, current.yearOfBirth);
+        return oldestAge<currentAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
