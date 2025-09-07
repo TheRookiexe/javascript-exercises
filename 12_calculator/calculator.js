3d59a31e176661da4e4d0dbb6d5222969de62d7d
TheRookiexe <mishrxa36@gmail.com>
2025-09-07 17:19:56 +0530

    All Tests Passed in Calculator Exercise.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let s= arr.reduce((acc, ele)=>acc+ele, 0);
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = arr.reduce((mul_acc, n)=>mul_acc*n, 1);
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(n,p) {
+	return n**p;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n==0 || n==1){
+    return 1;
+  }
+  return n*factorial(n-1);
 };
 
 // Do not edit below this line
