9968f042ab43441e285ab9da208281857c76546c
TheRookiexe <mishrxa36@gmail.com>
2025-09-07 21:04:14 +0530

    Fibonacci Exercise Completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n<0){
+        return 'OOPS'
+    }
+    n = Number(n);
+    let a=0;
+    let b = 1;
+    for (let i=0; i<n; i++){
+        let temp = a;
+        a = b;
+        b=temp+b;
+    }
+    return a;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
